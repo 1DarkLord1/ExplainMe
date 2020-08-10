2ff49be04bb5a2cd907ffe37854b4ec1f8ceca23
Denil Sharipov <32520090+1DarkLord1@users.noreply.github.com>
2020-08-10 20:38:33 +0300

    Delete Lecture.kt

--- app/src/main/java/com/explainme/Lecture.kt
+++
@@ -1,7 +0,0 @@
-package com.explainme
-
-class Lecture(val title: String,
-              val description: String,
-              val author: String,
-              val time: Int,
-              val url: String)
